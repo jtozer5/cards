242798f13867b14cd9117a3cfc8dd12243f48783
jtozer5 <joshiantozer@gmail.com>
2018-08-09 12:17:37 -0600

    new card function created

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
